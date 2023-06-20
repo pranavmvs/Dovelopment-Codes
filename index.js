415ed7986e6122fdea0ecd657aaf7e81b61f9698
Pranav Mvs <114419810+pranavmvs@users.noreply.github.com>
2023-06-20 21:43:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+function calculateArea(length, width) {
+  var area = length * width;
+  return area;
+}
 var length = 5;
 var width = 3;
-var area = length * width;
+var area = calculateArea(length, width);
 console.log("The area of the rectangle is: " + area);
