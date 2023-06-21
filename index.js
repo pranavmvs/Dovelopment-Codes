6b290bd0feb16ffeba2a8c273894ca676e7475fc
Pranav Mvs <114419810+pranavmvs@users.noreply.github.com>
2023-06-21 19:07:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-function calculateArea(length, width) {
-  var area = length * width;
-  return area;
+function findMax(numbers) {
+  if (!Array.isArray(numbers) || numbers.length === 0) {
+    return undefined;
+  }
+  let max = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  return max;
 }
-var length = 5;
-var width = 3;
-var area = calculateArea(length, width);
-console.log("The area of the rectangle is: " + area);
+const numbers = [55, 18, 82, 10, 39];
+const maxNumber = findMax(numbers);
+console.log(maxNumber);
